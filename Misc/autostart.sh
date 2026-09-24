3628ec112d67adb362fbf5009768d290a9998260
Pixel Prowler <68132166+LiuWoodsCode@users.noreply.github.com>
2026-09-24 05:10:11 -0400

    Add sample autostart

---
+++ Misc/autostart.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+python3 /home/dapixelprowler/libitina/Spawner/main.py /home/dapixelprowler/libitina/YuiHirasawa/main.py &
+python3 /home/dapixelprowler/libitina/Spawner/main.py /home/dapixelprowler/libitina/YuiHirasawa/notification.py &
+python3 /home/dapixelprowler/libitina/Spawner/main.py squeekboard &
